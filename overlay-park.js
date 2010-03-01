2f2b4b11972d6a73d01fb319c286fd06afd5a3cc
Sean Kelly <github@katron.org>
2010-03-01 16:31:10 -0600

    Update the BIP grapher files
    Now it's mostly working! Firefox only right now. Chrome is the next
    browser to get support.

--- overlay-park.js
+++ overlay-park.js
@@ -1,80 +1,38 @@
-function ParkBIP() {
-    this.park_bip = undefined;
-    this.park_from = undefined;
-    this.park_on = 'default';
-    this.selected_bip = {
-        'fly out': false, 'line out': false, 'ground out': false,
-        'single': false, 'double': false,
-        'triple': false, 'home run': false
-    };
-}
-
-ParkBIP.prototype.update_park = function(which, value) {
-    if (which == 'on') {
-        this.park_on = value;
-        this.redraw();
-    }
-    else if (which == 'from') {
-        this.park_from = value;
-        this.get_bip();
-        this.draw_bip();
-    }
-}
-
-ParkBIP.prototype.set_bip = function(which, state) {
-    this.selected_bip[which] = state;
-}
-
-ParkBIP.prototype.update_bip = function(which, state) {
-    this.set_bip(which, state);
-    if (state == false)
-        this.redraw();
-    else
-        this.draw_bip(which);
-}
-
-ParkBIP.prototype.get_bip = function() {
-}
-
-ParkBIP.prototype.draw_park = function() {
-    if (this.park_on == undefined)
-        return;
-    var canvas = document.getElementById('park-map');
-    var ctx = canvas.getContext('2d');
-    var width = canvas.width;
-    var height = canvas.height;
-    var img = new Image();
-    img.onload = function () {
-        ctx.drawImage(img, 0, 0, width, height);
-    }
-    img.src = 'img/' + this.park_on + '.png';
-}
-
-ParkBIP.prototype.draw_bip = function(bip) {
-    if (this.park_from == undefined)
-        return;
-}
-
-ParkBIP.prototype.redraw = function() {
-    this.draw_park();
-
-    if (this.park_bip != undefined)
-        this.draw_bip();
-}
-
-
-var bip = new ParkBIP();
+var bip;
 
 function initialize() {
     $("input").each(function(i, E) {
-        set_bip(E);
+        //set_bip(E);
     });
 
-    bip.redraw();
+    msgbox();
+    $("#strerror").ajaxError(function() {
+        msgbox("Fetch failed");
+    });
+
+    var canvas = document.getElementById('park-map');
+    if (!canvas.getContext) {
+        msgbox("A browser that supports the canvas element is required. Known browsers to support it include Chrome, Firefox, Opera, and Safari.");
+        return;
+    }
+
+    bip = new ParkBIP(canvas);
+    fetch_parks();
+    update_bip();
+}
+
+function msgbox(msg) {
+    var box = $("#strerror");
+    if (msg) {
+        box.text(msg).show();
+    }
+    else {
+        box.hide();
+    }
 }
 
 function toggle_canvas() {
-    var canvas = document.getElementById('park-map');
+    var canvas = bip.canvas;
     var toggle = $("#canvas-wrapper a");
     if (canvas == undefined || toggle == undefined)
         return;
@@ -88,16 +46,173 @@ function toggle_canvas() {
         canvas.width = 250;
         toggle.text("Expand park map");
     }
-
-    bip.redraw();
+    bip.draw();
 }
 
-function update_parks(option, which) {
-    bip.update_park(which, option.value);
+function update_filter(arg) {
+    var update = {};
+    function m(v) { return (v != "all" ? v : undefined); }
+    if (arg.pitcher) {
+        update.pitcher = m(arg.pitcher.value);
+    }
+    if (arg.batter) {
+        update.batter = m(arg.batter.value);
+    }
+
+    bip.update(update);
 }
 
+function update_bip(arg) {
+    var update = {};
+    var id_map = {
+        'bip_fo': 'fly out', 'bip_lo': 'line out', 'bip_go': 'ground out',
+        'bip_1b': 'single', 'bip_2b': 'double',
+        'bip_3b': 'triple', 'bip_hr': 'home run'
+    };
 
-for (i in [0, 1]) {
-    func = ['set', 'update'][i] + "_bip";
-    eval("function " + func + "(check){ id_map = { 'bip_fo': 'fly out', 'bip_lo': 'line out', 'bip_go': 'ground out', 'bip_1b': 'single', 'bip_2b': 'double', 'bip_3b': 'triple', 'bip_hr': 'home run', }; bip." + func + "(id_map[check.id], check.checked); }");
+    f = function(e) {
+        var el = $(e) || $(this);
+        var type = id_map[el.attr('id')];
+        update[type] = el.attr('checked');
+    }
+
+    if (arg) {
+        f(arg);
+    }
+    else {
+        $("input[id^='bip_']").each(f);
+    }
+
+    bip.update({ bip: update });
+}
+
+function update_park(arg) {
+    var update = {};
+    if (arg.from) {
+        var id = arg.from.value;
+        update.park_from = id;
+        if (bip.bip_exists(id))
+            populate_filter_list(id);
+        else
+            fetch_bip(id);
+    }
+    if (arg.on) {
+        update.park_on = arg.on.value;
+    }
+
+    bip.update(update);
+}
+
+function fetch_parks() {
+    $.get("bip/parks.xml", function(xml) {
+        var parks = { };
+        $(xml).find("park").each(function() {
+            var el = $(this);
+            var id = el.attr("id");
+            var park = {
+                id: id,
+                hp_x: el.attr("hp_x"),
+                hp_y: el.attr("hp_y"),
+                name: el.attr("name"),
+                scale: el.attr("scale")
+            };
+            parks[id] = park;
+        });
+
+        populate_parks_list(parks);
+        bip.add_parks(parks);
+    });
+}
+
+function fetch_bip(id, update) {
+    var url = "bip/park-" + id + ".xml";
+    bip.fetching(true);
+    $.get(url, function(xml) {
+        parse_bip(id, xml);
+        populate_filter_list(id);
+        bip.fetching(false);
+    });
+}
+
+function parse_bip(id, xml) {
+    var park = $(xml).find("park");
+    var json = {
+        batters: { },
+        pitchers: { }
+    };
+
+    json.bip = new Array();
+    // Scale things for the 250x250 image.
+    var image = bip.get_park_factors(id);
+    if (!image) {
+        msgbox("Couldn't get image scale factors for park id " + id + ".");
+        return;
+    }
+    image.scale *= 2;
+    image.hp_x /= 2;
+    image.hp_y /= 2;
+    park.children().each(function() {
+        var el = $(this);
+        var bip = {
+            x: (el.attr('x') - image.hp_x) * image.scale,
+            y: (image.hp_y - el.attr('y')) * image.scale,
+            'event': el.attr('event').toLowerCase(),
+            batter: el.attr('batter'),
+            pitcher: el.attr('pitcher'),
+        };
+
+        if (json.batters[bip.batter])
+            json.batters[bip.batter]++;
+        else
+            json.batters[bip.batter] = 1;
+
+        if (json.pitchers[bip.pitcher])
+            json.pitchers[bip.pitcher]++;
+        else
+            json.pitchers[bip.pitcher] = 1;
+
+        json.bip.push(bip);
+    });
+
+    bip.add_bip(id, json);
+}
+
+function populate_parks_list(parks) {
+    var from = $("#park-from");
+    var on = $("#park-on");
+
+    // parks is a mapping of id => stuff
+    // I need a mapping of park => id, but sorted
+    var sorted_parks = { };
+    var list = new Array();
+    for (var id in parks) {
+        sorted_parks[parks[id].name] = id;
+        list.push(parks[id].name);
+    }
+    list.sort(function(a, b) { return a.toLowerCase() > b.toLowerCase(); });
+
+    var park_insert = '<option selected="selected" disabled="disabled">Select a park</option>';
+    for (var i = 0; i < list.length; i++) {
+        park_insert += '<option value="' + sorted_parks[list[i]] + '">' + list[i] + '</option>';
+    }
+
+    from.empty();
+    from.append(park_insert);
+    on.empty();
+    on.append(park_insert);
+}
+
+function populate_filter_list(id) {
+    which = function(w, element) {
+        var list = bip.get_players(id, w);
+        element.empty();
+        var insert_text = '<option value="all">All</option>';
+        for (var i = 0; i < list.length; i++) {
+            insert_text += '<option value="' + list[i] + '">' + list[i] + '</option>';
+        }
+        element.append(insert_text);
+    }
+
+    which('pitchers', $("#pitcher-filter"));
+    which('batters', $("#batter-filter"));
 }
---
+++ parkbip.js
@@ -0,0 +1,170 @@
+function ParkBIP(canvas) {
+    this.canvas = canvas;
+    this._fetching = false,
+    this.fetch_queue = [ ],
+    this.filter = {
+        pitcher: undefined,
+        batter: undefined
+    };
+    this._park = {
+        on: 'default',
+        from: undefined
+    };
+    this.selected_bip = {
+        'fly out': true, 'line out': true, 'ground out': true,
+        'single': true, 'double': true, 'triple': true, 'home run': true
+    };
+}
+
+/**********/
+
+ParkBIP.prototype.update = function(arg) {
+    var updated = false;
+    if (!arg)
+        return;
+    if (arg.park_from) {
+        this._park.from = arg.park_from;
+        updated = true;
+    }
+    if (arg.park_on) {
+        this._park.on = arg.park_on;
+        updated = true;
+    }
+    if ("pitcher" in arg) {
+        this.filter.pitcher = arg.pitcher;
+        updated = true;
+    }
+    if ("batter" in arg) {
+        this.filter.batter = arg.batter;
+        updated = true;
+    }
+    if (arg.bip) {
+        for (var type in arg.bip) {
+            if (type in this.selected_bip) {
+                this.selected_bip[type] = arg.bip[type];
+            }
+        }
+        updated = true;
+    }
+
+    if (updated)
+        this.draw();
+}
+
+ParkBIP.prototype.fetching = function(are_fetching) {
+    if (are_fetching) {
+        this._fetching = true;
+    }
+    else {
+        this._fetching = false;
+        this.draw();
+    }
+}
+
+/**********/
+
+ParkBIP.prototype.add_parks = function(arg) {
+    // Add in the 'default' park
+    arg['default'] = {};
+    for (var id in arg) {
+        this._park[id] = arg[id];
+        var img = new Image();
+        img.src = 'img/' + id + '.png';
+        this._park[id].image = img;
+    }
+}
+
+ParkBIP.prototype.add_bip = function(id, bip) {
+    if (this._park[id] instanceof Object) {
+        for (var prop in bip) {
+            this._park[id][prop] = bip[prop];
+        }
+    }
+    else {
+        this._park[id] = bip;
+    }
+}
+
+ParkBIP.prototype.bip_exists = function(id) {
+    if (this._park[id] && this._park[id].bip instanceof Object)
+        return true;
+    return false;
+}
+
+ParkBIP.prototype.get_players = function(id, type, player) {
+    var list = new Array();
+    if (!this._park[id] || !this._park[id][type])
+        return list;
+
+    for (var name in this._park[id][type]) {
+        if (player) {
+            if (player == name)
+                list.push(name);
+        }
+        else
+            list.push(name);
+    }
+
+    list.sort();
+    return list;
+}
+
+ParkBIP.prototype.get_park_factors = function(id) {
+    if (this._park[id]) {
+        return {
+            hp_x: this._park[id].hp_x,
+            hp_y: this._park[id].hp_y,
+            scale: this._park[id].scale
+        };
+    }
+}
+
+/**********/
+
+ParkBIP.prototype.draw = function() {
+    if (!this._fetching) {
+        this.draw_park();
+        this.draw_bip();
+    }
+}
+
+ParkBIP.prototype.draw_park = function() {
+    if (this._park.on == undefined || this._park[this._park.on] == undefined)
+        return;
+    var ctx = this.canvas.getContext('2d');
+    var width = this.canvas.width;
+    var height = this.canvas.height;
+    var img = this._park[this._park.on].image;
+    ctx.drawImage(img, 0, 0, width, height);
+}
+
+ParkBIP.prototype.draw_bip = function(bip) {
+    if (this._park.on == undefined || this._park.on == "default" || this._park.from == undefined)
+        return;
+
+    var park_on = this._park[this._park.on];
+    var pitcher = this.filter.pitcher;
+    var batter = this.filter.batter;
+    var ctx = this.canvas.getContext('2d');
+
+    var scale = parseFloat(park_on.scale);
+    var hp_x = parseFloat(park_on.hp_x);
+    var hp_y = parseFloat(park_on.hp_y);
+
+    if (this.canvas.height == 250) {
+        scale *= 2;
+        hp_x /= 2;
+        hp_y /= 2;
+    }
+
+    var bip_list = this._park[this._park.from].bip;
+    for (var i = 0; i < bip_list.length; i++) {
+        var bip = bip_list[i];
+        if (!this.selected_bip[bip.event] || (pitcher && pitcher != bip.pitcher) || (batter && batter != bip.batter))
+            continue;
+
+        ctx.beginPath();
+        ctx.arc(bip.x/scale + hp_x, hp_y - bip.y/scale, 2, 0, 2*Math.PI, false);
+        ctx.fill();
+    }
+}
