09596ae77447679a721dbfef22baa98b4861c674
Sean Kelly <github@katron.org>
2012-07-04 00:00:27 -0500

    Use the image_hp_* fields when drawing
    I think this is the right way to do it. This means the hp_* used when
    parsing the bip assumes the park has not been shifted. When drawing, use
    the image_hp_* fields because the park will be shifted.

--- parkbip.js
+++ parkbip.js
@@ -140,8 +140,8 @@ var K = (function(my, $) {
             }
             if (image_year > 0) {
                 return {
-                    hp_x: this._park[id].images[image_year].hp_x,
-                    hp_y: this._park[id].images[image_year].hp_y,
+                    hp_x: this._park[id].images[image_year].image_hp_x,
+                    hp_y: this._park[id].images[image_year].image_hp_y,
                     scale: this._park[id].images[image_year].scale,
                     file: this._park[id].images[image_year].file
                 };
