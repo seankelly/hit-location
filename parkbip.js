8752a17fed0da829259f898831d97b5c9236e1d2
Sean Kelly <github@katron.org>
2012-10-21 15:21:08 -0500

    Greatly simplify the indirection in returning the park factors
    There were four lookups just to get to the final lookup. Those four
    lookups were constant though, so stash it in a variable and base the
    lookups off that variable. I doubt the speed will increase much, if at
    all, but readability should be improved.

--- parkbip.js
+++ parkbip.js
@@ -139,13 +139,14 @@ var K = (function(my, $) {
                 }
             }
             if (image_year > 0) {
+                var park = this._park[id].images[image_year];
                 return {
-                    hp_x: this._park[id].images[image_year].hp_x,
-                    hp_y: this._park[id].images[image_year].hp_y,
-                    image_hp_x: this._park[id].images[image_year].image_hp_x,
-                    image_hp_y: this._park[id].images[image_year].image_hp_y,
-                    scale: this._park[id].images[image_year].scale,
-                    file: this._park[id].images[image_year].file
+                    hp_x: park.hp_x,
+                    hp_y: park.hp_y,
+                    image_hp_x: park.image_hp_x,
+                    image_hp_y: park.image_hp_y,
+                    scale: park.scale,
+                    file: park.file
                 };
             }
         }
